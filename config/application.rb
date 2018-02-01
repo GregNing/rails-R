97a79c6e9cc6b897e041b9a3254cba6449065cb5
Greg <GregJieNing@gmail.com>
2018-02-01 18:28:59 +0800

    add chinese

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module RailsRecipes
   class Application < Rails::Application
+    #配置中文語系
+    config.i18n.default_locale = "zh-TW"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
