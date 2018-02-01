7cc24ca515fbb05c0af7082a3dcacb86dbf3b5e9
Greg <GregJieNing@gmail.com>
2018-02-01 23:15:13 +0800

    add ticket

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :memberships
   has_one :profile
+  #Rails 的 accepts_nested_attributes_for 作用是可以在更新 User 时，
+  #也顺便可以更新 Profile 资料。
+  accepts_nested_attributes_for :profile
   has_many :groups, :through => :memberships
   def display_name
     self.email.split("@").first
